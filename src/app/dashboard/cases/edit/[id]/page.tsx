96efe2328afa567f9ec30e058050d7b2045c22b6
miles <miles1618@gmail.com>
2024-11-14 15:52:49 -0600

    uploads to s3 working

--- src/app/dashboard/cases/edit/[id]/page.tsx
+++ src/app/dashboard/cases/edit/[id]/page.tsx
@@ -7,8 +7,6 @@ export default async function EditPage({params}: {
 
     return (
         <div>
-            <h1>Edit Page</h1>
-            <p>Editing case with id: {id}</p>
             <CaseForm id={id}/>
         </div>
     )
