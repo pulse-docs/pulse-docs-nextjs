7efa1a3194bbf2e4f1fddb0add003ade2a88a429
miles <miles1618@gmail.com>
2024-12-04 09:19:19 -0600

    update search for MUI

--- src/app/components/dashboard/common/search.tsx
+++ src/app/components/dashboard/common/search.tsx
@@ -3,6 +3,7 @@
 import { Search as SearchIcon } from '@mui/icons-material';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+import { TextField, InputAdornment } from '@mui/material';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -20,20 +21,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathName}?${params.toString()}`);
   }, 500);
-  
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get('query')?.toString()}
+      <TextField
+          variant="outlined"
+          placeholder={placeholder}
+          onChange={(e) => handleSearch(e.target.value)}
+          defaultValue={searchParams.get('query')?.toString()}
+          InputProps={{
+            startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+            ),
+          }}
+          fullWidth
       />
-        <SearchIcon />
-    </div>
   );
 }
