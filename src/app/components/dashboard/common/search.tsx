a2fab351a5a2dac4b17b02142624dd4bd9b48c46
miles <miles1618@gmail.com>
2024-12-04 09:39:28 -0600

    update dropdown and toggles for MUI

--- src/app/components/dashboard/common/search.tsx
+++ src/app/components/dashboard/common/search.tsx
@@ -6,36 +6,38 @@ import { useDebouncedCallback } from 'use-debounce';
 import { TextField, InputAdornment } from '@mui/material';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-  const pathName = usePathname();
-  const { replace } = useRouter();
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    const { replace } = useRouter();
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
+    const handleSearch = useDebouncedCallback((term: string) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
 
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathName}?${params.toString()}`);
-  }, 500);
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
+        replace(`${pathName}?${params.toString()}`);
+    }, 500);
 
-  return (
-      <TextField
-          variant="outlined"
-          placeholder={placeholder}
-          onChange={(e) => handleSearch(e.target.value)}
-          defaultValue={searchParams.get('query')?.toString()}
-          InputProps={{
-            startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-            ),
-          }}
-          fullWidth
-      />
-  );
+    return (
+        <TextField
+            variant="outlined"
+            placeholder={placeholder}
+            onChange={(e) => handleSearch(e.target.value)}
+            defaultValue={searchParams.get('query')?.toString()}
+            slotProps={{
+                input: {
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                },
+            }}
+            fullWidth
+        />
+    );
 }
