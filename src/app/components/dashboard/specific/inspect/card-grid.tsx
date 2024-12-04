65ae0e816c1de0b383acb7b5eedfdb21e632411b
miles <miles1618@gmail.com>
2024-12-04 09:55:43 -0600

    update card-case and card-grid for MUI

--- src/app/components/dashboard/specific/inspect/card-grid.tsx
+++ src/app/components/dashboard/specific/inspect/card-grid.tsx
@@ -1,25 +1,28 @@
 import { Case } from '@/app/utils/definitions';
-import {MedicalCard} from '@/app/components/dashboard/specific/inspect/card-medical';
-import {getCaseByFuzzy, getCaseByFuzzyMRI, getCasesByFileId} from "@/app/lib/controllers/cases";
+import { MedicalCard } from '@/app/components/dashboard/specific/inspect/card-medical';
+import { getCaseByFuzzy } from "@/app/lib/controllers/cases";
+import { Grid2 as Grid } from '@mui/material';
 
-export async function CardGrid({query, topK, filter, showTreatment, fullSearch}: {query: string, topK: number, filter: string, showTreatment: boolean, fullSearch: boolean}) {
-    const data: Case[] = await getCaseByFuzzy({query, topK, filter, fullSearch}) || [];
+export async function CardGrid({ query, topK, filter, showTreatment, fullSearch }: { query: string, topK: number, filter: string, showTreatment: boolean, fullSearch: boolean }) {
+    const data: Case[] = await getCaseByFuzzy({ query, topK, filter, fullSearch }) || [];
 
     return (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
+        <Grid container spacing={3}>
             {data.map((card, index) => (
-                <MedicalCard key={index}
-                             claim_id={card.claim_id}
-                             matches={card.matches}
-                             matched={card.matched}
-                             score={card.score}
-                             extent_of_injury={card.extent_of_injury}
-                             method_of_injury={card.method_of_injury}
-                             file_id={card.file_id}
-                             treatment={card.treatment}
-                             showTreatment={showTreatment}
-                />
+                <Grid size={{ sm:12, md: 6, lg: 3}} key={index}>
+                    <MedicalCard
+                        claim_id={card.claim_id}
+                        matches={card.matches}
+                        matched={card.matched}
+                        score={card.score}
+                        extent_of_injury={card.extent_of_injury}
+                        method_of_injury={card.method_of_injury}
+                        file_id={card.file_id}
+                        treatment={card.treatment}
+                        showTreatment={showTreatment}
+                    />
+                </Grid>
             ))}
-        </div>
+        </Grid>
     );
-    }
+}
